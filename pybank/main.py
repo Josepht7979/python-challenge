34aeb0ab322fbb59a9b1221e4d293e05f5e17979
josepht7979 <josepht7979@gmail.com>
2021-08-26 23:29:04 +1000

    Uploading all results including the modified code

--- pybank/main.py
+++ pybank/main.py
@@ -1,19 +1,61 @@
 import os
-
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
-with open(csvpath) as csvfile:
+pyBank = os.path.join("Resources","budget_data.csv")
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+with open (pyBank, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_headers = next(csvreader, None)
 
-    print(csvreader)
+    months = []
+    profit = []
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        months.append(row[1])
+        profit.append(int(row[1]))
+
+    total_months = len(months)
+    print (f"Total Months: {total_months}")
+    
+#------------------------------------------
+    
+    net_total = 0
+    i = 1
+    for i in range(total_months):
+        net_total = net_total + int(profit[i])
+    print (f"Total: ${net_total}")
+
+#------------------------------------------
+# Average change per month
+# change in profit/loss column per month
+    change = []
+    j = 0
+    k = 0
+
+    for j in range (1, total_months):
+        if j == 0:
+            change.append(0)
+        else:
+            change.append(int(profit[j])-int(profit[k]))
+            k += 1
+#---------------------------------------------------------
+# Add monthly changes and divide by total_months
+    average_monthly_change = ((sum(change))/(len(change)))
+    print (f"Average Change: ${round((average_monthly_change),2)}")
+
+#---------------------------------------------------------   
+# greatest increase in profits
+    max_change = max(change)
+    print (f"Greatest Increase in Profits: ${max_change}")
+# greatest decrease in profits
+    min_change = min(change)
+    print (f"Greatest Decrease in Profits:  ${min_change}")
+
+
+# Print to text file
+with open('Results.txt', "w") as text:
+        text.write(f"Total Months: {total_months}\n")
+        text.write(f"Total: ${net_total}\n")
+        text.write(f"Average Change: ${round((average_monthly_change))}\n")
+        text.write(f"Greatest Increase in Revenues: ${max_change}\n")
+        text.write(f"Greatest Decrease in Revenues:  ${min_change}\n")
